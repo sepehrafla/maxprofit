2df1f07cbed022f728cdd949a9622eea09047450
Sepehrafla <saflatounian@gmail.com>
2019-08-02 13:23:28 -0400

    second

--- filemaxstock.py
+++ filemaxstock.py
@@ -7,11 +7,21 @@ import yfinance as yf
 from lists import *
 
 prices = []
+dates = []
 initial_buyin = 1000
 x = 0
 y = 0
 z = 0
 t = 0
+
+def get_data (filename):
+    with open(filename, 'r') as csvfile:
+        csvFileReader = csv.reader(csvfile)
+        next(csvFileReader)
+        for row in csvFileReader:
+            dates.append(int(row[0].split('-')[0]))
+            prices.append(float(row[1]))
+    return
 
 def maxProfit (prices):
     sell1 = 0
@@ -34,16 +44,11 @@ def maxProfit (prices):
         z= int(y/(sell1+buy2))
         t= z*(sell2+buy2)
     except: pass
+    print (buy1,sell1,buy2)
     return sell2
 
-for name in fname:
-    satas = yf.download (name, '2019-07-01', '2019-08-01')
-    df = pd.DataFrame(satas)
-    prices = df ['Close']
-    solution = maxProfit(prices)
-    print (name)
-    file1 = open("myfile.txt","a")#append mode 
-    file1.write(str(t)+'\n') 
-    file1.close() 
+
+get_data('')
+maxProfit(prices)
     
 
